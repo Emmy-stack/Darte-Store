876a6ae84e16f172dc95e20451c6e95d2e3eb0e3
Atunwa Emmanuel <atunwaemmanuel@Atunwas-MacBook-Air.local>
2026-06-09 12:12:39 +0100

    Updated project files

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -5,8 +5,8 @@ const Newsletter = () => {
     return (
         <div className='flex flex-col items-center mx-4 my-36'>
             <Title title="Join Newsletter" description="Subscribe to get exclusive deals, new arrivals, and insider updates delivered straight to your inbox every week." visibleButton={false} />
-            <div className='flex bg-slate-100 text-sm p-1 rounded-full w-full max-w-xl my-10 border-2 border-white ring ring-slate-200'>
-                <input className='flex-1 pl-5 outline-none' type="text" placeholder='Enter your email address' />
+            <div className='flex bg-zinc-100 dark:bg-zinc-800/40 text-sm p-1 rounded-full w-full max-w-xl my-10 border-2 border-white dark:border-zinc-700/30 ring ring-zinc-200/50 dark:ring-zinc-800/50'>
+                <input className='flex-1 pl-5 !bg-transparent !border-none outline-none text-slate-700 dark:text-slate-200' type="text" placeholder='Enter your email address' />
                 <button className='font-medium bg-green-500 text-white px-7 py-3 rounded-full hover:scale-103 active:scale-95 transition'>Get Updates</button>
             </div>
         </div>
